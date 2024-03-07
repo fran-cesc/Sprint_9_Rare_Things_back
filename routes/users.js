38a0c91cbbf0d3ae949ebb5f0043033eeeec6906
Cesc <francesc9000@gmail.com>
2024-03-07 13:42:53 +0100

    register working

--- routes/users.js
+++ routes/users.js
@@ -8,24 +8,59 @@ dotenv.config();
 // Database connection
 const {connection} = require("../config/config.db");
 
+// Get all users
+const getAllUsers = (request, response) => {
+  connection.query("SELECT * FROM users", (error, results) => {
+    if (error) {        
+        return response.status(500).json({ error: " Server Error. Could not retrieve all users" });
+    }
+    response.status(200).json(results);
+  });
+};
+
+// Route
+app.route("/users").get(getAllUsers);
+
+// Get user
+const getUser = (request, response) => {
+  const email = request.params.email;
+  console.log(email);
+  connection.query(
+    "SELECT * from users WHERE email = ?",
+    [email],
+    (error, results) => {
+        if (error) {        
+            return response.status(500).json({ error: " Server Error. Could not retrieve user" });
+        }
+        if (results.length === 0) { 
+          console.log(results);
+          return response.status(200).json(results);
+        }
+        return response
+        .status(200)
+        .json(results[0]);
+    }
+  );
+}
+
+// Route
+app.route("/users/:email").get(getUser);
+
 
 // Register user
-
 const registerUser = (request, response) => {
   const { user_name, email, password } = request.body;
-  console.log(request.body.value)
-  console.log(request.body)
   connection.query(
-    "INSERT INTO users(user_name, email, password) VALUES (?,?,?)",
+    "INSERT INTO users (user_name, email, password) VALUES (?,?,?)",
     [user_name, email, password],
     (error, results) => {
       if (error) {
-        console.log(error);
         return response.status(500).json({ error: " Server Error. User could not be registered" });
       }
-      response
+      return response
         .status(201)
-        .json({ message: "User registered successfully", userId: results.insertId });
+        .json({ message: "User registered successfully"});
+        // .json({ message: "User registered successfully", userId: results.insertId, user_name: results.user_name });
     }
   );
 };
@@ -34,10 +69,8 @@ const registerUser = (request, response) => {
 app.route("/register").post(registerUser);
 
 // User login
-
 const logUser = (request, response) => {
-  const { email, password } = request.body;
-  debugger;
+  const { email, password, user_name } = request.body;
   connection.query(
     "SELECT user_name, email, password from users where email=?",
     [email],
@@ -45,12 +78,9 @@ const logUser = (request, response) => {
       if (error) {
         return response.status(500).json({ error: " Server Error. User could not be registered" });
       }
-      
-      if (results.length <= 0 || results[0].password != password ) {
+      if (results.length <= 0 || results[0].password != password) {
         return response.status(401).json({ message: "Incorrect username or password"});
       }
-      console.log(email);
-      console.log(request.body);
       const accessToken = jwt.sign(request.body, process.env.ACCESS_TOKEN, {
         expiresIn: "2h",
       })
@@ -64,25 +94,7 @@ const logUser = (request, response) => {
 //Route
 app.route("/login").post(logUser);
 
-// // Delete user
-// const delUser = (request, response) => {
-//   const id = request.params.id;
-//   connection.query(
-//     "Delete from users where id = ?",
-//     [id],
-//     (error, results) => {
-//       if (error){
-//         return response.status(500).json({ error: " Server Error. User could not be added" });
-//       };
-//       response
-//         .status(201)
-//         .json({ "User removed successfully": results.affectedRows });
-//     }
-//   );
-// };
 
-// // Route
-// app.route("/users/:id").delete(delUser);
 
 
 module.exports = app;
