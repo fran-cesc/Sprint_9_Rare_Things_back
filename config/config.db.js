0e76b3c579ab1d139f24466bc5d8d4a4d1078812
Cesc <francesc9000@gmail.com>
2024-11-01 08:09:51 +0100

    production changes

--- config/config.db.js
+++ config/config.db.js
@@ -1,17 +1,16 @@
-require("dotenv").config();
-
 const mysql = require('mysql');
 let connection;
 
 try {
     connection = mysql.createConnection({
-        host: process.env.DBHOST,
-        user: process.env.DBUSER,
-        password: process.env.DBPASS,
-        database: process.env.DBNAME
+        host: process.env.MYSQLHOST,           // Host proporcionado por Railway
+        user: process.env.MYSQLUSER,           // Usuario proporcionado por Railway
+        password: process.env.MYSQLPASSWORD,   // Contraseña proporcionada por Railway
+        database: process.env.MYSQLDATABASE,   // Nombre de la base de datos proporcionado por Railway
+        port: process.env.MYSQLPORT || 3306,   // Puerto proporcionado por Railway o 3306 por defecto
     });
 } catch (error) {
-    console.log("Error connecting database");
+    console.log("Error connecting to the database:", error);
 }
 
-module.exports = {connection};
+module.exports = { connection };
