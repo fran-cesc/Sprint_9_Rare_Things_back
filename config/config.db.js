844e3f6c1769baa596dd75040e022dfce2444ea9
Cesc <francesc9000@gmail.com>
2024-11-20 11:55:25 +0100

    auto-ping

--- config/config.db.js
+++ config/config.db.js
@@ -2,13 +2,6 @@ const mysql = require('mysql');
 let connection;
 
 try {
-    console.log({
-        host: process.env.MYSQL_ADDON_HOST,
-        user: process.env.MYSQL_ADDON_USER,
-        password: process.env.MYSQL_ADDON_PASSWORD,
-        database: process.env.MYSQL_ADDON_DB,
-        port: process.env.MYSQL_ADDON_PORT || 3306,
-    });
     connection = mysql.createConnection({
         host: process.env.MYSQL_ADDON_HOST,
         user: process.env.MYSQL_ADDON_USER,
--- server.js
+++ server.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const cors = require('cors');
 const fs = require('node:fs');
+const https = require('https');
+const API_URL = 'https://rare-things-back.onrender.com/things';
+
 
 require('dotenv').config();
 
@@ -29,5 +32,15 @@ app.use(express.static('img_uploads'));
 app.listen(process.env.PORT || 10000, () => {
     console.log(`Server running at port ${process.env.PORT || 10000}`);
 });
+
+// Auto-ping every 5 min to keep server awake
+setInterval(()=> {
+https.get(API_URL, (resp) => {
+    console.log(`self-ping status: ${resp.statusCode}`);
+}).on('error', (error) => {
+    console.error('self-ping failed', error.message);
+});
+}, 5*60*1000);
+
 
 module.exports = app;
