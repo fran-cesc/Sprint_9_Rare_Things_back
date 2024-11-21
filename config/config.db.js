28a0107b6fccfd1ccd3d6f8a80ff3289f1c38163
Cesc <francesc9000@gmail.com>
2024-11-21 07:59:00 +0100

    ready for deploy on render

--- config/config.db.js
+++ config/config.db.js
@@ -5,21 +5,21 @@ let connection;
 try {
 
     // CLEVER CLOUD
-    // connection = mysql.createConnection({
-    //     host: process.env.MYSQL_ADDON_HOST,
-    //     user: process.env.MYSQL_ADDON_USER,
-    //     password: process.env.MYSQL_ADDON_PASSWORD,
-    //     database: process.env.MYSQL_ADDON_DB,
-    //     port: process.env.MYSQL_ADDON_PORT || 3306,
-    // });
+    connection = mysql.createConnection({
+        host: process.env.MYSQL_ADDON_HOST,
+        user: process.env.MYSQL_ADDON_USER,
+        password: process.env.MYSQL_ADDON_PASSWORD,
+        database: process.env.MYSQL_ADDON_DB,
+        port: process.env.MYSQL_ADDON_PORT || 3306,
+    });
 
     //LOCALHOST
-    connection = mysql.createConnection({
-        host: process.env.DBHOST,
-        user: process.env.DBUSER,
-        password: process.env.DBPASS,
-        database: process.env.DBNAME
-    });
+    // connection = mysql.createConnection({
+    //     host: process.env.DBHOST,
+    //     user: process.env.DBUSER,
+    //     password: process.env.DBPASS,
+    //     database: process.env.DBNAME
+    // });
 
 } catch (error) {
     console.log("Error connecting to the database:", error);
--- server.js
+++ server.js
@@ -1,8 +1,8 @@
 const express = require('express');
 const cors = require('cors');
 const fs = require('node:fs');
-// const https = require('https');
-// const API_URL = 'https://rare-things-back.onrender.com/things';
+const https = require('https');
+const API_URL = 'https://rare-things-back.onrender.com/things';
 
 
 require('dotenv').config();
@@ -10,15 +10,14 @@ require('dotenv').config();
 const app = express();
 
 // Middleware Render
-// app.use(cors({
-//     origin: 'https://rare-things.vercel.app', 
-//     methods: ['GET', 'POST', 'PUT', 'DELETE'],
-//     allowedHeaders: ["Content-Type", "Authorization"],
-//     credentials: true 
-// }));
+app.use(cors({
+    origin: 'https://rare-things.vercel.app', 
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    allowedHeaders: ["Content-Type", "Authorization"],
+    credentials: true 
+}));
 
-//Middleware local
-app.use(cors());
+
 app.use(express.json({ limit: '5mb' }));
 app.use(express.urlencoded({ extended: true, limit: '5mb' }));
 
@@ -32,23 +31,23 @@ app.use(require('./routes/comments'));
 app.use(express.static('img_uploads'));
 
 // Initialize server
-// app.listen(process.env.PORT || 10000, () => {
-//     console.log(`Server running at port ${process.env.PORT || 10000}`);
-// });
-
-// Initialize server in local
-app.listen(process.env.PORT || 3000, () => {
-    console.log(`Server running at port ${process.env.PORT}`);
+app.listen(process.env.PORT || 10000, () => {
+    console.log(`Server running at port ${process.env.PORT || 10000}`);
 });
 
-// Auto-ping every 10 min to keep server awake
-// setInterval(()=> {
-// https.get(API_URL, (resp) => {
-//     console.log(`self-ping status: ${resp.statusCode}`);
-// }).on('error', (error) => {
-//     console.error('self-ping failed', error.message);
+// Initialize server in local
+// app.listen(process.env.PORT || 3000, () => {
+//     console.log(`Server running at port ${process.env.PORT}`);
 // });
-// }, 10*60*1000);
+
+// Auto-ping every 10 min to keep server awake
+setInterval(()=> {
+https.get(API_URL, (resp) => {
+    console.log(`self-ping status: ${resp.statusCode}`);
+}).on('error', (error) => {
+    console.error('self-ping failed', error.message);
+});
+}, 10*60*1000);
 
 
 module.exports = app;
