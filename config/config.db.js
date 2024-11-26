2de2b0316ed0d81afc19a343c2f72ff80b4e0b2b
Cesc <francesc9000@gmail.com>
2024-11-26 09:10:37 +0100

    added db connection init

--- config/config.db.js
+++ config/config.db.js
@@ -4,13 +4,23 @@ let connection;
 
 try {
 
-    // CLEVER CLOUD
+    // CLEVER CLOUD setup connection
     connection = mysql.createConnection({
         host: process.env.MYSQL_ADDON_HOST,
         user: process.env.MYSQL_ADDON_USER,
         password: process.env.MYSQL_ADDON_PASSWORD,
         database: process.env.MYSQL_ADDON_DB,
         port: process.env.MYSQL_ADDON_PORT || 3306,
+    });
+
+      // Try to connect
+      connection.connect(err => {
+        if (err) {
+            console.error("Error connecting to the database:", err);
+            process.exit(1); // Optionally stop the process if the connection fails
+        } else {
+            console.log("Database connected successfully.");
+        }
     });
 
     //LOCALHOST
